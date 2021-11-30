dc03e99aec8e3df9636651922b17faa155d0ba59
andrewhunt21 <andrewhunt21@icloud.com>
2021-11-30 14:44:47 -0800

    basic dom complete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+      <div>
+        <h1>GitHub Info</h1>
+
+        <form>
+          <input />
+          <button>Search</button>
+        </form>
+
+        <div>
+          <img />
+          <h2>Andrew Hunt</h2>
+          <p>(andrewhunt21)</p>
+          <p>Total Repos: lots</p>
+          <p>Total Followers: millions</p>
+        </div>
+
+        <div>
+          <h2>Followers:</h2>
+          <img />
+          <p>usernames</p>
+        </div>
+
+      </div>);
   }
 }
 
